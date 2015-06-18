eb8e466dae1b3b993ca772aa354e66df5ed15e04
longthan01 <longthan385922@gmail.com>
2015-06-18 20:36:18 +0700

    thêm ado.net
    thêm ado.net

---
+++ ado.net/ENVNconnector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ado.net
+{
+    public class ENVNconnector
+    {
+        public List<string> getListTenChuDe()
+        {
+            string connStr = "Data Source=./SQLSERVER2K12;Initial Catalog=ENVN;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connStr);
+            string sqlQuery = "select * from thematic"; // sql query
+
+            List<string> danhSachTenChuDe = new List<string>(); // list lưu danh sách tên chuyên đề
+
+            if (conn != null) // nếu biến conn != null     -> kết nối csdl thành công
+            {
+                Console.Write("Ket noi CSDL ok");
+                conn.Open();
+                SqlCommand command = new SqlCommand(sqlQuery, conn);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read()) // chỉ đọc khi reader còn có dữ liệu
+                {
+                    string thematicName = reader["ThematicName"].ToString();
+                    danhSachTenChuDe.Add(thematicName);
+                }
+            }
+            else
+            {
+                Console.Write("\nKet noi CSDL that bai, xem lai connection string ");
+            }
+            return danhSachTenChuDe;
+        }
+    }
+}
---
+++ ado.net/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ado.net
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ENVNconnector connector = new ENVNconnector();
+                List<string> dsTenChuyenDe = connector.getListTenChuDe();
+                Console.WriteLine("\nDanh sach chuyen de : \n");
+                foreach (string s in dsTenChuyenDe)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\n\nCo loi, xem lai chuong trinh, SQL server bat len chua?");
+            }
+        }
+    }
+}
